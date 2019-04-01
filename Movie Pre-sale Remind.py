0897298c99a090da528726fe764b33a406bad52f
Duerxin <ericsysu16@gmail.com>
2019-04-01 15:15:10 +0800

    添加信息发送功能

--- Movie Pre-sale Remind.py
+++ Movie Pre-sale Remind.py
@@ -5,7 +5,7 @@ from fake_useragent import UserAgent
 import time
 import random
 import os
-import json
+from twilio.rest import Client
 
 class PreSaleQuery():
     def __init__(self, cinemaId, movieName):
@@ -13,6 +13,7 @@ class PreSaleQuery():
         self.movieName = movieName
         self.onsale_movies = []
         self.checkTimes = 2
+        self.message = ""
 
     def getPage(self):
         target = 'https://maoyan.com/cinema/'+str(self.cinemaId)
@@ -20,7 +21,7 @@ class PreSaleQuery():
         # Fake User Agent
         ua = UserAgent(verify_ssl=False)
         header = {"User-Agent": ua.random}
-        req = requests.get(url=target, headers=header, proxies=self.getProxy())
+        req = requests.get(url=target, headers=header, proxies=self.getProxy(),timeout=10)
         html = req.text
         bf = BeautifulSoup(html, 'html.parser')
         cinema_title_html = bf.find_all('h3', class_='name text-ellipsis')
@@ -32,6 +33,7 @@ class PreSaleQuery():
         print("> 检查时间：", formatTime)
         print("> 电影：", self.movieName)
         print("> 影院：", cinema_title_html[0].get_text())
+        self.message = cinema_title_html[0].get_text() + " 《" + self.movieName + "》 开售啦！"
 
     def getProxy(self):
         api = requests.get("http://ip.jiangxianli.com/api/proxy_ip")
@@ -59,6 +61,18 @@ class PreSaleQuery():
             print(fuzz_result[0], "模糊查询最匹配结果！相似度：", fuzz.partial_ratio(self.movieName, fuzz_result[0]))
             return True
 
+    def messaging(self):
+
+        # Your Account SID from twilio.com/console
+        account_sid = "x"
+        # Your Auth Token from twilio.com/console
+        auth_token = "x"
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            to="+xxx",
+            from_="+xxx",
+            body=self.message)
+
     def loopCheck(self):
         ps.getPage()
         while not ps.accurateCheck() and not ps.fuzzCheck():
@@ -67,11 +81,15 @@ class PreSaleQuery():
 
             print("= 第{}次查询".format(self.checkTimes))
             ps.getPage()
+        self.messaging()
+        return
+#
 
 
 if __name__ == '__main__':
     cinemaID = 14409
     search_movie = '复仇者联盟4：终局之战'
+    # search_movie = '小飞象'
     ps = PreSaleQuery(cinemaID, search_movie)
     ps.loopCheck()
 
