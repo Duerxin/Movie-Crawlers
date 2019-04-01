547917e05f01cff5a0ba4776c178a9baf56dfa5a
Duerxin <ericsysu16@gmail.com>
2019-04-01 14:22:32 +0800

    循环查询

--- Movie Pre-sale Remind.py
+++ Movie Pre-sale Remind.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz, process
 from fake_useragent import UserAgent
 import time
+import random
+import os
 
 class PreSaleQuery():
     def __init__(self, cinemaId, movieName):
         self.cinemaId = cinemaId
         self.movieName = movieName
         self.onsale_movies = []
+        self.checkTimes = 2
 
     def getPage(self):
         target = 'https://maoyan.com/cinema/'+str(self.cinemaId)
@@ -24,7 +27,6 @@ class PreSaleQuery():
         self.onsale_movies = []
         for movie in onsale_movies_html:
             self.onsale_movies.append(movie.get_text())
-
         formatTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print("> 检查时间：", formatTime)
         print("> 电影：", self.movieName)
@@ -49,12 +51,20 @@ class PreSaleQuery():
             print(fuzz_result[0], "模糊查询最匹配结果！相似度：", fuzz.partial_ratio(self.movieName, fuzz_result[0]))
             return True
 
+    def loopCheck(self):
+        ps.getPage()
+        while not ps.accurateCheck() and not ps.fuzzCheck():
+            time.sleep(random.randint(5,30))
+            os.system("cls")
+
+            print("= 第{}次查询".format(self.checkTimes))
+            ps.getPage()
+
+
 if __name__ == '__main__':
     cinemaID = 14409
-    search_movie = '比悲伤更悲伤的故事'
+    search_movie = '雷霆沙赞'
     ps = PreSaleQuery(cinemaID, search_movie)
-    ps.getPage()
-    if not ps.accurateCheck():
-        ps.fuzzCheck()
+    ps.loopCheck()
 
 
